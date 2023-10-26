b61ad1acb38a5484bca0d92011e52f0fc387df5e
Rafiul <rafiulbinoy@gmail.com>
2023-10-26 17:52:38 +0600

    bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:test_geet/aboutListTile/AboutListTile.dart';
-import 'package:test_geet/alertDilogue/AlertDialogue.dart';
-import 'package:test_geet/animatedAlign/WAnimatedAlign.dart';
-import 'package:test_geet/wscube_widgets/wcontainer.dart';
+import 'package:test_geet/floating_button/w_floating_button.dart';
 import 'package:test_geet/wscube_widgets/wimages.dart';
 
-import 'align/WAlign.dart';
-import 'http/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +21,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const WImages());
+        home: const WFloatingButton());
   }
 }
