1be730b49bd23274eb80946f4dc27d153d2c8119
Rafiul <rafiulbinoy@gmail.com>
2023-10-30 11:58:53 +0600

    FadeInImage work is done wimages

--- lib/wscube_widgets/wimages.dart
+++ lib/wscube_widgets/wimages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class WImages extends StatefulWidget {
   const WImages({super.key});
@@ -11,11 +12,33 @@ class _WImagesState extends State<WImages> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       body: Center(
-          child: SizedBox(
-              width: 250,
-              height: 250,
-              child: Image.asset("assets/images/road.jpg"))),
+        child: FadeInImage.assetNetwork(
+          placeholder: "assets/images/place_holder.png",
+          image: "assets/images/road.jpg",
+        ),
+      ),
+
+
+      // body: Stack(
+      //   children: <Widget>[
+      //     const Center(
+      //       child: CircularProgressIndicator(),
+      //     ),
+      //     Center(
+      //       child: FadeInImage.memoryNetwork(
+      //           placeholder: kTransparentImage,
+      //           image: "assets/images/road.jpg"),
+      //     )
+      //   ],
+      // ),
+
+      // body: Center(
+      //     child: SizedBox(
+      //         width: 250,
+      //         height: 250,
+      //         child: Image.asset("assets/images/road.jpg"))),
     );
   }
 }
